7dd81ef7002fca0e29d6407ac35f60b473c1c94d
Anjali <anjali13.aj@gmail.com>
2026-05-24 18:10:11 +0530

    List of PlaylistIds

--- video_stats.py
+++ video_stats.py
@@ -26,7 +26,36 @@ def get_playlist():
  
  except requests.exceptions.RequestException as e:
    raise e
- 
+
+
+
+def get_video_ids(playlist_id):
+  videos_ids=[]
+  pageToken= None
+  base_url=f"https://youtube.googleapis.com/youtube/v3/playlistItems?part=contentDetails&maxResults=50&playlistId=UUX6OQ3DkcsbYNE6H8uQQuVA&key={API_KEY}"   
+  try:
+    while True:
+      url=base_url
+      if pageToken:
+        url+=f"&pageToken={pageToken}"
+      response=requests.get(url)
+      response.raise_for_status()
+      data=response.json()  
+      for item in data.get("items",[]):
+        video_id=item["contentDetails"]["videoId"]
+        videos_ids.append(video_id)
+      pageToken=data.get("nextPageToken")
+
+      if pageToken is None:
+        break
+
+    return videos_ids    
+
+  except requests.exceptions.RequestException as e:
+    raise e  
+  
+  
 if __name__=="__main__":
-  get_playlist()
+  playlist_id=get_playlist() 
+  get_video_ids(playlist_id)
  
